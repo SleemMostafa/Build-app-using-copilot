30f0f347f529787f4340dc3016e95251c2decc13
Sleem Mostafa <sleemmostafa1995@gmail.com>
2025-08-24 00:31:55 +0300

    test run project

--- CoffeeRestaurant.Application/obj/Debug/net9.0/CoffeeRestaurant.Application.AssemblyInfo.cs
+++ CoffeeRestaurant.Application/obj/Debug/net9.0/CoffeeRestaurant.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoffeeRestaurant.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a828b69d1455aa7a189bb65b888b67f52092c79")]
 [assembly: System.Reflection.AssemblyProductAttribute("CoffeeRestaurant.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CoffeeRestaurant.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
